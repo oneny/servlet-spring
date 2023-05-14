0e35b7878b16954233c0cd581d935e066e705cff
oneny <gjwodnjs1996@gmail.com>
2023-05-15 07:17:44 +0900

    first commit

---
+++ src/test/java/oneny/sevlet/SevletApplicationTests.java
@@ -0,0 +1,13 @@
+package oneny.sevlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
